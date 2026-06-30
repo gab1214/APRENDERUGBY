b4b11fd26070df4a00fd4fe1fe2508871c154752
gab1214 <ga.olguins@duocuc.cl>
2026-06-30 00:56:22 -0400

    Update UsuarioController.java

--- Producto/proyecto_aprende_rugby/proyecto_aprende_rugby/src/main/java/aprenderugby/proyecto_aprende_rugby/controller/UsuarioController.java
+++ Producto/proyecto_aprende_rugby/proyecto_aprende_rugby/src/main/java/aprenderugby/proyecto_aprende_rugby/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package aprenderugby.proyecto_aprende_rugby.controller;
+
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,21 +19,22 @@ import aprenderugby.proyecto_aprende_rugby.services.UsuarioServices;
 
 @RestController
 @RequestMapping("/api/usuarios")
+// Permitimos que tu Vercel acceda de forma segura a este controlador
+@CrossOrigin(origins = "https://aprenderrugby-k1hy.vercel.app") 
 public class UsuarioController {
-
 
     private final UsuarioServices usuarioService;
 
     public UsuarioController(UsuarioServices usuarioService) {
         this.usuarioService = usuarioService;
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> obtenerPorId(@PathVariable Long id) {
         return usuarioService.buscarPorId(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-
 
     @PostMapping
     public Usuario registrar(@RequestBody Usuario usuario) {
@@ -41,18 +45,22 @@ public class UsuarioController {
     public List<Usuario> listar() {
         return usuarioService.listar();
     }
+
     @PostMapping("/login")
-public ResponseEntity<String> login(@RequestBody Map<String, String> datos) {
-    String email = datos.get("email");
-    String password = datos.get("password");
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> datos) {
+        String email = datos.get("email");
+        String password = datos.get("password");
 
-    boolean valido = usuarioService.validarLogin(email, password);
+        boolean valido = usuarioService.validarLogin(email, password);
 
-    if (valido) {
-        return ResponseEntity.ok("Login exitoso");
-    } else {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
+        if (valido) {
+            
+            Map<String, String> respuesta = new HashMap<>();
+            respuesta.add("status", "success");
+            respuesta.add("email", email);
+            return ResponseEntity.ok(respuesta);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
-}
-
 }
